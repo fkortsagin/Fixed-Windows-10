be2a5f66a3ef32c90979b644d7a8ecb0ccec3009
gimalaji_blake <fjodor.kortsagin@gmail.com>
2021-02-12 09:35:29 +0200

    Add files via upload

---
+++ additional debloat.ps1
@@ -0,0 +1,23 @@
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.DesktopAppInstaller_2018.720.2137.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.HEIFImageExtension_1.0.11792.0_x64__8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.MicrosoftOfficeHub_2017.1219.520.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.MicrosoftStickyNotes_2.0.13.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.MixedReality.Portal_2000.18081.1242.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.Office.OneNote_16001.10228.20003.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.ScreenSketch_2018.731.48.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.VP9VideoExtensions_1.0.12342.0_x64__8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.WindowsStore_11805.1001.4913.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.Xbox.TCUI_1.11.28003.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.XboxApp_41.41.18001.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.XboxGameOverlay_1.32.17005.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.XboxIdentityProvider_12.44.20001.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.XboxSpeechToTextOverlay_1.17.29001.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.WebpImageExtension_1.0.11551.0_x64__8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.YourPhone_2018.727.2137.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.WindowsCamera_2018.425.120.0_neutral_~_8wekyb3d8bbwe
+Remove-ProvisionedAppxPackage -Online -PackageName Microsoft.XboxGamingOverlay_2.20.22001.0_neutral_~_8wekyb3d8bbwe
+
+
+
+
+
